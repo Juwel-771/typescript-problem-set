32d181ac7a9463a0a12eecb505f0d7181710b62e
Juwel-771 <juwelah99@gmail.com>
2025-11-16 23:56:33 +0600

    problem 2 and 4 solved

--- solution.js
+++ solution.js
@@ -8,9 +8,9 @@ function formatValue(value) {
     }
     return !value;
 }
-console.log(formatValue("Hello"));
-console.log(formatValue(5));
-console.log(formatValue(true));
+// console.log(formatValue("Hello"));
+// console.log(formatValue(5));
+// console.log(formatValue(true));
 // ? Problem 2
 function getLength(data) {
     if (typeof data === "string") {
@@ -21,7 +21,34 @@ function getLength(data) {
     }
     return 0;
 }
-console.log(getLength('Hello World')); // Output: 11
-console.log(getLength([1, 2, 3, 4, 5])); // Output: 5
-console.log(getLength('')); // Output: 0
-console.log(getLength([])); // Output: 0
+// console.log(getLength('Hello World'));       // Output: 11
+// console.log(getLength([1, 2, 3, 4, 5]));    // Output: 5
+// console.log(getLength(''));                  // Output: 0
+// console.log(getLength([]));                  // Output: 0
+// ? Problem 3:
+var Person = /** @class */ (function () {
+    function Person(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    Person.prototype.getDetails = function () {
+        return "'Name: ".concat(this.name, ", Age: ").concat(this.age, "'");
+    };
+    return Person;
+}());
+function filterByRating(items) {
+    var result = [];
+    for (var _i = 0, items_1 = items; _i < items_1.length; _i++) {
+        var item = items_1[_i];
+        if (item.rating >= 4) {
+            result.push(item);
+        }
+    }
+    return result;
+}
+var books = [
+    { title: 'Book A', rating: 4.5 },
+    { title: 'Book B', rating: 3.2 },
+    { title: 'Book C', rating: 5.0 },
+];
+console.log(filterByRating(books));
--- solution.ts
+++ solution.ts
@@ -33,3 +33,62 @@ function getLength(data: string | any[]):number{
 // console.log(getLength([1, 2, 3, 4, 5]));    // Output: 5
 // console.log(getLength(''));                  // Output: 0
 // console.log(getLength([]));                  // Output: 0
+
+// ? Problem 3:
+class Person{
+    public name: string;
+    public age: number;
+
+    constructor(name: string, age: number){
+        this.name = name;
+        this.age = age;
+    }
+
+    getDetails():string{
+        return `'Name: ${this.name}, Age: ${this.age}'`
+    }
+}
+
+// const person1 = new Person('Juwel Meherab', 26);
+// console.log(person1.getDetails());
+
+// ? Problem 4:
+type Items = {
+    title: string;
+    rating: number;
+};
+
+function filterByRating(data: Items[]): Items[]{
+    
+    const resultValue: Items[] = [];
+
+    for(let item of data){
+        if(item.rating >= 4){
+            resultValue.push(item);
+        }
+    }
+
+    return resultValue;
+
+}
+
+// const books = [
+//   { title: 'Book A', rating: 4.5 },
+//   { title: 'Book B', rating: 3.2 },
+//   { title: 'Book C', rating: 5.0 },
+// ];
+
+// console.log(filterByRating(books));
+
+// ? Problem 5:
+type User = {
+    id: number;
+    name: string; 
+    email: string;
+    isActive: boolean;
+}
+
+function filterActiveUsers(userData: User[]):User[]{
+    return userData.filter(user=>user.isActive===true)
+}
+
